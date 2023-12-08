d6b5efde35917b5acb99c39892160c0f6c31a512
Evgeniy Doroshenko <kraspy@yandex.ru>
2023-12-08 15:08:21 +0700

    js

---
+++ 00 Basic/variables.js
@@ -0,0 +1,23 @@
+var myVar0 = 1;
+let myVar1 = 2;
+const MY_CONST = 3;
+
+let a;
+let b;
+
+// Присваивание возвращает значение
+console.log((a = 1));
+
+var newVar;
+a = b = 2;
+
+// Математические операции
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// +=, -=, *=, /=, %=, **=
+// --a, a--, ++a, a++
